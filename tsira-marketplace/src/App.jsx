00b0fcd1884dbc3e7f9dbc9d317ba802042e63d7
USER <aishamuarin@gmail.com>
2026-04-06 00:59:31 +0100

    create folder and install initial package for tsira-marketplace projects

---
+++ tsira-marketplace/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <section>
+        <div className='text-3xl text-blue-500'>Hello | Welcome to Tsira-Market Place</div>
+      </section>
+        
+         
+         
+
+     
+    </>
+  )
+}
+
+export default App
