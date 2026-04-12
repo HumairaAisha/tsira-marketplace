c93fc9eb4c817d0df5ef6b84e2b47f91292f6693
USER <aishamuarin@gmail.com>
2026-04-12 22:42:17 +0100

    style(features): Add animation to features section using motion framer on landing page

--- tsira-marketplace/src/Sections/Features.jsx
+++ tsira-marketplace/src/Sections/Features.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable-next-line no-unused-vars */
+import  { motion } from "framer-motion"
+import {  popIn, slideUp  } from "../animations/motion"
+
 
 function Features() {
    const steps = ["Connecting buyers directly with sellers", "Providing a social-media-like experience for product discovery", "Enabling smooth and secure transactions",
@@ -8,11 +12,26 @@ function Features() {
     <section id="features" className="scroll-mt-20">
       <div className="h-full bg-tsira-bg px-8 md:px-10 py-28">
       <div className="space-y-2">
-      <h3 className="text-center text-3xl font-semibold text-tsira-primary">What Tsira Does </h3>
-      <p className="text-center text-tsira-muted text-sm md:text-base">Tsira bridges the gap between social interaction and digital commerce 
-      by</p>
+      <motion.h3 
+      variants={popIn}
+          initial="initial"
+          transition={{duration: 0.6}}
+          whileInView="whileInView"
+      className="text-center text-3xl font-semibold text-tsira-primary">What Tsira Does </motion.h3>
+      <motion.p 
+      variants={popIn}
+          initial="initial"
+          transition={{duration: 0.6}}
+          whileInView="whileInView"
+      className="text-center text-tsira-muted text-sm md:text-base">Tsira bridges the gap between social interaction and digital commerce 
+      by</motion.p>
       </div>
-      <div className="grid grid-cols-1 pt-10 md:grid-cols-2 lg:grid-cols-2 gap-8 px-8 md:px-10">
+      <motion.div 
+      variants={popIn}
+          initial="initial"
+          transition={{duration: 0.6}}
+          whileInView="whileInView"
+      className="grid grid-cols-1 pt-10 md:grid-cols-2 lg:grid-cols-2 gap-8 px-8 md:px-10">
       {steps.map((step, index) => (
          <div key={index} className="bg-tsira-primary/10 p-6 rounded-xl hover:shadow-md transition-shadow duration-300 flex flex-col hover:scale-[1.04]">
             <div className="flex gap-2 hover:cursor-pointer">
@@ -23,10 +42,15 @@ function Features() {
             </div>
          </div>
       ))}
-      </div>
+      </motion.div>
       <div>
-      <p className="text-tsira-muted py-4 px-8 md:px-10 md:text-base">Users can scroll through products just like they would on social 
-      platforms, making shopping more interactive and engaging.</p>
+      <motion.p 
+      variants={slideUp}
+          initial="initial"
+          transition={{duration: 0.6}}
+          whileInView="whileInView"
+      className="text-tsira-muted py-4 px-8 md:px-10 md:text-base">Users can scroll through products just like they would on social 
+      platforms, making shopping more interactive and engaging.</motion.p>
    </div>
    </div>
    
